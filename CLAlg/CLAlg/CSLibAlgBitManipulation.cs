c0a1d249f9ac42353aff9d9248c06e5cc3cd0668
ywang690 <james.frosty3150102@gmail.com>
2025-07-25 21:09:55 +0800

    136

---
+++ CLAlg/CLAlg/CSLibAlgBitManipulation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLAlg
+{
+    public class CSLibAlgBitManipulation
+    {
+        public bool SingleNumber()   //136
+        {
+            bool _output = false;
+            return _output;
+        }
+    }
+}
