92564a1432cbd19a985f89d6208347424a39183a
ywang690 <james.frosty3150102@gmail.com>
2026-02-16 12:30:14 +0800

    ++ 2,10,24,25,44,50,60,143,203,206,,224,231,233

--- CLAlg/CLAlg/CSLibAlgRecursion.cs
+++ CLAlg/CLAlg/CSLibAlgRecursion.cs
@@ -13,5 +13,70 @@ namespace CLAlg
             bool _result = false;
             return _result;
         }
+        public bool AddTwoNumbers() //2
+        {
+            bool _result = false;
+            return _result;
+        }
+        public bool RegularExpressionMatching() //10
+        {
+            bool _result = false;
+            return _result;
+        }
+        public bool SwapNodesinPairs() //24
+        {
+            bool _result = false;
+            return _result;
+        }
+        public bool ReverseNodesinKGroup() //25
+        {
+            bool _result = false;
+            return _result;
+        }
+        public bool WildcardMatching() //44
+        {
+            bool _result = false;
+            return _result;
+        }
+        public bool PowXN() //50
+        {
+            bool _result = false;
+            return _result;
+        }
+        public bool PermutationSequence() //60
+        {
+            bool _result = false;
+            return _result;
+        }
+        public bool ReorderList()   //143
+        {
+            bool _result = false;
+            return _result;
+        }
+        public bool RemoveLinkedListElements()  //203
+        {
+            bool _result = false;
+            return _result;
+        }
+        public bool ReverseLinkedList() //206
+        {
+            bool _result = false;
+            return _result;
+        }
+        public bool BasicCalculator() //224
+        {
+            bool _result = false;
+            return _result;
+        }
+        public bool PowerofTwo() //231
+        {
+            bool _result = false;
+            return _result;
+        }
+        public bool NumberofDigitOne()//233
+        {
+            bool _result = false;
+            return _result;
+        }
     }
 }
